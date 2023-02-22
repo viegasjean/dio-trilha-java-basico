0f680c3201a8c9ce1ec30df72c52c220ad64a03b
Jean Viegas <jeanmelg@gmail.com>
2023-02-22 14:43:38 -0300

    operadores logicos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+  public static void main(String[] args) {
+    boolean var = true;
+    System.out.println(!var);
+
+    String teste = var ? "verdade": "mentira";
+    System.out.println(teste);
+  }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        String meuNome = "Jean Viegas";
+
+        System.out.println(meuNome.length());
+
+        final double PI = 3.14;
+
+        System.out.println(PI);
     }
 }
